7e65250d65be3f4929d5a6338833ce3ae915636f
ichirohan <120529763+ichirohan@users.noreply.github.com>
2026-01-17 16:33:16 -0300

    Delete aula2.js

--- aula2.js
+++
@@ -1,8 +0,0 @@
-diaDasemana = prompt('Qual é o dia da semana?')
-if (diaDasemana == 'Sábado'){
-    alert('Bom fim de semana!');
- } else if (diaDasemana == 'Domingo'){
-    alert('Bom fim de semana!');
- } else {
-    alert('Boa semana!');
-}
